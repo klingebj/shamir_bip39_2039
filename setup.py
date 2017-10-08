7429939012f9e8147ce0406c38f925a7a6715745
Brad Klingenberg <bklingenberg@Brads-MacBook-Air.local>
2017-10-08 14:46:40 -0700

    refactoring word list

--- setup.py
+++ setup.py
@@ -5,5 +5,6 @@ setup(
     name='shamir_bip39_2039',
     version='0.0.1',
     test_suite='nose2.collector.collector',
-    packages=['shamir_bip39_2039']
+    packages=['shamir_bip39_2039'],
+    package_data={'shamir_bip39_2039': ['english.txt']}
 )
